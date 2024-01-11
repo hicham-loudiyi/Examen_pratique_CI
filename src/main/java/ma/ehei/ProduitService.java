abf24e61656003c9aff281da3a8e1f73e8052631
LOUDIYI Hicham <hicham.loud@gmail.com>
2024-01-11 13:12:56 +0100

    Dev fonctionnalite create

--- src/main/java/ma/ehei/ProduitService.java
+++ src/main/java/ma/ehei/ProduitService.java
@@ -19,4 +19,26 @@ public class ProduitService {
 		for(int i = 0; i < produits.size(); i++)
 			System.out.println(produits.get(i));
 	}
+    
+    public boolean produitExists(Long id, String nom) {
+        for (Produit produit : produits) {
+            if (produit.getId().equals(id) || produit.getNom().equals(nom)) {
+                return true;
+            }
+        }
+        return false; 
+    }
+
+    public void CreateProduit(Produit produit) throws IllegalArgumentException {
+        if (!produitExists(produit.getId(), produit.getNom())) {
+            if (produit.getPrix() >= 0 && produit.getQuantite() >= 0) {
+                produits.add(produit);
+                System.out.println("Produit ajouté : " + produit.getNom());
+            } else {
+                throw new IllegalArgumentException("Le prix et la quantité doivent être positifs pour la création du produit");
+            }
+        } else {
+            throw new IllegalArgumentException("Un produit avec le même Id ou nom existe déjà");
+        }
+    }
 }
