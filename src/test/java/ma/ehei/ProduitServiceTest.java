1a72d8a00dd5e42278e6ea3ccd9f818154021bd6
marouabekkaoui <126347037+maroua-bekkaoui@users.noreply.github.com>
2024-01-11 15:48:24 +0100

    Update ProduitServiceTest.java

--- src/test/java/ma/ehei/ProduitServiceTest.java
+++ src/test/java/ma/ehei/ProduitServiceTest.java
@@ -35,5 +35,42 @@ public class ProduitServiceTest {
         ps.UpdateProduit(updatedProduit);
         assertTrue(ps.ReadProduit(1L).equals(updatedProduit));
     }
+	@Test
+    public void testDeleteProduit() {
+        Produit produit = new Produit(1L,"omo",12,100);
+        ps.CreateProduit(produit);
+        ps.DeleteProduit(1L);
+        assertTrue(ps.ReadProduit(1L) == null);
+    }
+	
+	@Test
+	public void testProduitExists() {
+        Produit produit = new Produit(1L,"omo",12,100);
+        ps.CreateProduit(produit);
+        assertTrue(ps.produitExists(1L,"omo"));
+    }
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateExistingIdOrName() {
+        Produit produit1 = new Produit(1L,"omo",12,100);
+        Produit produit2 = new Produit(1L,"tide",12,100);
+        ps.CreateProduit(produit1);
+        ps.CreateProduit(produit2);
+    }
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateNegativePriceOrQuantity() {
+        Produit produit = new Produit(1L,"omo",-12,-100);
+        ps.CreateProduit(produit);
+    }
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testUpdateNegativePriceOrQuantity() {
+		Produit produit = new Produit(1L,"omo",12,100);
+        ps.CreateProduit(produit);
+        Produit updatedProduit = new Produit(1L,"omo",-12,-100);
+        ps.updateProduit(updatedProduit);
+    }
+	
 
 }
